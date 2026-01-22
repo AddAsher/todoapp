d898a337ba132f646846058180ca3b02762c3685
AddAsher <asheram2013@gmail.com>
2026-01-21 23:20:51 -0500

    css styling part 2 - active feature for classes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
   return (
     <div className="App">
       <h1>My Todos</h1>
 
-      <div className='todo-wrapper'>
-        <div className='todo-input'>
-          <div className = 'todo-input-item'>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
             <label>Title</label>
-            <input type="text" placeholder="What's the task title?"/>
+            <input type="text" placeholder="What's the task title?" />
           </div>
-          <div className = 'todo-input-item'>
+          <div className="todo-input-item">
             <label>Description</label>
-            <input type="text" placeholder="What's the task description?"/>
-          </div><div className = 'todo-input-item'>
-            <button type="button" className='primatyBtn'>Add</button>
-          </div>  
+            <input type="text" placeholder="What's the task description?" />
+          </div>
+          <div className="todo-input-item">
+            <button type="button" className="primaryBtn">
+              Add
+            </button>
+          </div>
         </div>
 
-        <div className='btn-area'>
-          <button>Todos</button>
-          <button>Completed</button>
+        <div className="btn-area">
+          <button
+            className={`secondaryBtn ${isCompleteScreen === false ? "active" : ""}`}
+            onClick={() => setIsCompleteScreen(false)}
+          >
+            Todos
+          </button>
+          <button
+            className={`secondaryBtn ${isCompleteScreen === true && "active"}`}
+            onClick={() => setIsCompleteScreen(true)}
+          >
+            Completed
+          </button>
         </div>
 
-        <div className='todo-list'>
-          <div className ="todo-list-item">
+        <div className="todo-list">
+          <div className="todo-list-item">
             <h3>Task 1</h3>
             <text>Description</text>
           </div>
